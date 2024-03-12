2e74892bcc3ad9933be9da9e7843a1347534a142
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-03-12 08:29:46 +0100

    use stable abi in launcher

--- launcher.cpp
+++ launcher.cpp
@@ -1,74 +1,33 @@
+#define Py_LIMITED_API 3
 #include <Python.h>
-#include <filesystem>
 
-void check_status(PyStatus& status, PyConfig& config) {
-    if (PyStatus_Exception(status)) {
-        PyConfig_Clear(&config);
-        if (PyStatus_IsExit(status)) {
-            exit(status.exitcode);
-        }
-        Py_ExitStatusException(status);
-    }
-}
+#include <filesystem>
+#include <iostream>
 
 int main(int argc, char** argv) {
-    PyStatus status;
-    PyConfig config;
-
-    // Run python in isolated mode, i.e. :
-    //      - don't add "unsafe" paths to sys.path
-    //      - ignore env vars
-    //      - ignore user site directory
-    PyConfig_InitIsolatedConfig(&config);
-
-    auto bundle_root = std::filesystem::path(argv[0]).parent_path();
-    auto app_main = bundle_root / "app" / "main.py";
+    auto exe = std::filesystem::path(argv[0]);
+    auto bundle_root = exe.parent_path();
+    auto app_root = bundle_root / "app";
+    auto app_main = app_root / "main.py";
     auto python_home = bundle_root / "python";
     auto python_dlls = python_home / "DLLs";
     auto python_lib = python_home / "Lib";
 
-    // Set home directory to prevent python from adding its default sys.path.
-    // With this sys.path will only contain :
-    //      - paths manually added with PyWideStringList_Append()
-    //      - existing site-packages directories, in our case :
-    //          - the python directory
-    //          - <python directory>/Lib/site-packages
-    status = PyConfig_SetString(&config, &config.home,
-                                python_home.wstring().c_str());
-    check_status(status, config);
+    auto exe_str = exe.wstring();
+    auto app_main_str = app_main.wstring();
+    auto python_home_str = python_home.wstring();
 
-    // Tell Py_InitializeFromConfig() to not modify config.module_search_paths.
-    config.module_search_paths_set = 1;
+#pragma warning(push)
+#pragma warning(disable : 4996)
+    Py_SetProgramName(exe_str.c_str());
+    Py_SetPythonHome(python_home_str.c_str());
+#pragma warning(pop)
 
-    // Add python DLLs to sys.path.
-    status = PyWideStringList_Append(&config.module_search_paths,
-                                     python_dlls.wstring().c_str());
-    check_status(status, config);
+    Py_Initialize();
 
-    // Add standard python libs to sys.path.
-    status = PyWideStringList_Append(&config.module_search_paths,
-                                     python_lib.wstring().c_str());
-    check_status(status, config);
+    const int args_size = 4;
+    wchar_t* args[args_size] = {exe_str.data(), L"-E", L"-s",
+                                app_main_str.data()};
 
-    std::filesystem::path script{argc == 1 ? app_main : argv[1]};
-    if (std::filesystem::exists(script)) {
-        // Add app directory to sys.path.
-        status =
-            PyWideStringList_Append(&config.module_search_paths,
-                                    script.parent_path().wstring().c_str());
-        check_status(status, config);
-
-        // Run target script instead of interpreter's main.
-        status = PyConfig_SetString(&config, &config.run_filename,
-                                    script.wstring().c_str());
-        check_status(status, config);
-    }
-
-    status = Py_InitializeFromConfig(&config);
-    check_status(status, config);
-    PyConfig_Clear(&config);
-
-    // Run config.run_filename if it's set.
-    // If not, run the interpreter.
-    return Py_RunMain();
+    Py_Main(args_size, args);
 }
