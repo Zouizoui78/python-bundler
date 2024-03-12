d79d896c0c06d8c153799ac4496e56c0bf2c39e6
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-03-12 12:01:42 +0100

    launcher cleanup

--- launcher.cpp
+++ launcher.cpp
@@ -1,33 +1,36 @@
 #define Py_LIMITED_API 3
-#include <Python.h>
+#include "Python.h"
 
 #include <filesystem>
-#include <iostream>
 
 int main(int argc, char** argv) {
-    auto exe = std::filesystem::path(argv[0]);
-    auto bundle_root = exe.parent_path();
-    auto app_root = bundle_root / "app";
-    auto app_main = app_root / "main.py";
-    auto python_home = bundle_root / "python";
-    auto python_dlls = python_home / "DLLs";
-    auto python_lib = python_home / "Lib";
+    std::filesystem::path exe_path = std::filesystem::path(argv[0]);
+    std::filesystem::path bundle_root = exe_path.parent_path();
 
-    auto exe_str = exe.wstring();
-    auto app_main_str = app_main.wstring();
-    auto python_home_str = python_home.wstring();
+    std::wstring exe = exe_path.wstring();
+    std::wstring app_main = (bundle_root / "app" / "main.py").wstring();
+    std::wstring python_home = (bundle_root / "python").wstring();
 
 #pragma warning(push)
 #pragma warning(disable : 4996)
-    Py_SetProgramName(exe_str.c_str());
-    Py_SetPythonHome(python_home_str.c_str());
+    // These two functions are deprecated since python 3.11.
+    // However being part of the stable ABI they will continue to work.
+    // See last paragraph of https://peps.python.org/pep-0652/#stable-abi
+    // The worst case scenario is that they can end up leaking memory,
+    // but that's not an issue here since they are only used once.
+    Py_SetProgramName(exe.c_str());
+    Py_SetPythonHome(python_home.c_str());
 #pragma warning(pop)
 
     Py_Initialize();
 
     const int args_size = 4;
-    wchar_t* args[args_size] = {exe_str.data(), L"-E", L"-s",
-                                app_main_str.data()};
+    wchar_t* args[] = {exe.data(),
+                       L"-E", // Ignore PYTHON* environment variables.
+                       L"-s", // Don't add user site directory to sys.path.
+                       app_main.data()};
 
-    Py_Main(args_size, args);
+    // Don't pass the last argument if app/main.py doesn't exist.
+    // In that case the interpreter will show its interactive prompt.
+    Py_Main(std::filesystem::exists(app_main) ? args_size : 3, args);
 }
